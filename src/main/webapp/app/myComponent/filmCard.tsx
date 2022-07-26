5b79874602261d39fcc532543ca12e302521d7d7
marco <delucamarco>
2022-07-26 14:24:02 +0200

    inserita configurazione email in dev

--- src/main/webapp/app/myComponent/filmCard.tsx
+++ src/main/webapp/app/myComponent/filmCard.tsx
@@ -1,6 +1,6 @@
 import { useAppDispatch, useAppSelector } from 'app/config/store';
 import { getEntities } from 'app/entities/film/film.reducer';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { Card } from 'reactstrap';
 import LocandinaFilm from './locandinaFilm/locandinaFilm';
 
@@ -9,7 +9,6 @@ import DescrizioneFilm from './locandinaFilm/descrizioneFilm';
 
 const FilmCard = () => {
   const dispatch = useAppDispatch();
-  const [firstRender, setFirstRender] = useState(true);
 
   const filmList = useAppSelector(state => state.film.entities);
   const filmActive = filmList.filter(film => {
@@ -23,7 +22,12 @@ const FilmCard = () => {
   return (
     <Card style={{ marginBottom: '1rem' }} className="filmCard">
       <LocandinaFilm immagine={filmActive[0]?.urlImg} />
-      <DescrizioneFilm titolo={filmActive[0]?.titolo} data={filmActive[0]?.dataUscita} descrizione={filmActive[0]?.descrizione} />
+      <DescrizioneFilm
+        titolo={filmActive[0]?.titolo}
+        data={filmActive[0]?.dataUscita}
+        descrizione={filmActive[0]?.descrizione}
+        dataFine={filmActive[0]?.dataFineIscrizione}
+      />
     </Card>
   );
 };
